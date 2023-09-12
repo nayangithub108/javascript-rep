dac0afa407f733b8c6117e17569bcd77a6a26571
nayangithub108 <133083964+nayangithub108@users.noreply.github.com>
2023-09-12 12:29:56 +0000

    date-time

---
+++ basicJs/date-time.js
@@ -0,0 +1,22 @@
+let date = new Date ();
+// //console.log(date.toDateString())
+// console.log(date.toISOString());
+// console.log(date.toLocaleDateString());
+// console.log(date.toString());
+
+// ***********************************
+// we can set the date and time also
+// let newdate = new Date(2023,0,23)
+// let newdate = new Date (2023,0,23,4,5)
+// console.log(newdate.toLocaleString());
+
+let anotherdate = new Date("2013-1-26")
+// console.log(anotherdate.toLocaleString())
+// console.log(anotherdate.toDateString())
+
+// *************************************************
+// now time 
+
+let timeSpan = Date.now() 
+// console.log(timeSpan);
+// console.log(anotherdate.getTime());
